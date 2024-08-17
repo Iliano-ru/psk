1cf75e78853e91131a0240241bbb037bdc3fb2e7
iliano <n_i_d@bk.ru>
2024-08-17 14:45:58 +0400

    cutaway

---
+++ styles/components/header/burger/burger.js
@@ -0,0 +1,5 @@
+﻿let burger = document.querySelector(".burger");
+let burger__span = document.querySelector('.burger__span');
+burger.onclick = () => {
+    burger__span.classList.toggle('burger__span_active')
+}
---
+++ styles/components/header/menu-burger/menu-burger.js
@@ -0,0 +1,12 @@
+﻿const burger = document.querySelector(".burger");
+const burger__span1 = document.querySelector('.burger__span1');
+const burger__span2 = document.querySelector('.burger__span2');
+const burger__span3 = document.querySelector('.burger__span3');
+const menu = document.querySelector('.menu');
+burger.onclick = () => {
+    burger.classList.toggle('burger_active');
+    burger__span1.classList.toggle('burger__span1_active');
+    burger__span2.classList.toggle('burger__span2_active');
+    burger__span3.classList.toggle('burger__span3_active');
+    menu.classList.toggle('menu_active');
+}
