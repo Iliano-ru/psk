a6002b2b9b1c78a26a8afeae4007d17d2946a189
iliano <n_i_d@bk.ru>
2024-08-30 14:53:29 +0400

    spoiler

---
+++ styles/components/footer/footer.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const spoiler = document.querySelector('.spoiler');
+    const footerLogo = document.querySelector('.footer__inner')
+    const checkMark = document.querySelector('.check-mark')
+    spoiler.onclick = () => {
+        footerLogo.classList.toggle('showed')
+        checkMark.classList.toggle('check-mark_active')
+    }
+})
