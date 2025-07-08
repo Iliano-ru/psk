edfec43a5a4ca51de4b07af0738d4d1e5979ed67
Ilia <n_i_d@bk.ru>
2025-07-08 22:56:31 +0400

    good-templates-configuration

--- landing/urls.py
+++ landing/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about_us/', views.about_us),
+    path('expertise/', views.expertise),
+    path('careers/', views.careers),
+    path('responsibility/', views.responsibility),
+    path('lets_meet/', views.lets_meet),
+    path('contact/', views.contact),
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "landing/index.html")
+    return render(request, "landing/index.html", {'path': request.path})
+
+def about_us(request):
+    return render(request, "landing/index.html", {'path': request.path})
+
+def expertise(request):
+    return render(request, "landing/index.html", {'path': request.path})
+
+def careers(request):
+    return render(request, "landing/index.html", {'path': request.path})
+
+def responsibility(request):
+    return render(request, "landing/index.html", {'path': request.path})
+
+def lets_meet(request):
+    return render(request, "landing/index.html", {'path': request.path})
+
+def contact(request):
+    return render(request, "landing/index.html", {'path': request.path})
